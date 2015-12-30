cd339d7b76d98e2c6cdfad72dbde6f40573de3ae
Jouko Johansson <joukojo@gmail.com>
2015-12-30 12:33:20 +0200

    guava common.math

---
+++ src/main/java/org/yogocodes/guava/examples/common/math/LongMathExample.java
@@ -0,0 +1,11 @@
+package org.yogocodes.guava.examples.common.math;
+
+import com.google.common.math.LongMath;
+
+public class LongMathExample {
+	public static void main(String[] args) {
+		System.out.println(LongMath.gcd(512L, 2048L));
+		System.exit(0);
+		
+	}
+}
