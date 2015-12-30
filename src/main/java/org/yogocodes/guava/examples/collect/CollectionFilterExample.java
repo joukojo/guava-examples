a6efa6a737b5a1660d852e71b9914783deb13a17
Jouko Johansson <joukojo@gmail.com>
2015-12-30 13:18:59 +0200

    added guava sort example

--- src/main/java/org/yogocodes/guava/examples/collect/CollectionFilterExample.java
+++ src/main/java/org/yogocodes/guava/examples/collect/CollectionFilterExample.java
@@ -2,20 +2,25 @@ package org.yogocodes.guava.examples.collect;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.Ordering;
 
 public class CollectionFilterExample {
 	public static void main(String[] args) {
 		filteringExample();
 		
+		transformGuavaExample();
 		
-		sortingExample();
+		sortGuavaExample();
+		sortingExampleJava8();
 	}
 
 	private static void filteringExample() {
@@ -47,7 +52,36 @@ public class CollectionFilterExample {
 		return personnel;
 	}
 	
-	public static void sortingExample() {
+	public static void transformGuavaExample() {
+		List<Person> personnel = createPersonnelList();
+		Function<Person, String> function = sortFunction();
+		Collection<String> sortedCollection = Collections2.transform(personnel, function );
+		
+		System.out.println(sortedCollection);
+		
+	}
+	
+	public static void sortGuavaExample() {
+		List<Person> personnel = createPersonnelList();
+
+		Collections.sort(personnel, Ordering.natural().onResultOf( new Function<Person, Integer>() {
+		      public Integer apply(Person from) {
+		          return from.getId();
+		        }
+		      }));
+		
+		System.out.println("guava sort" + personnel);
+	}
+
+	private static Function<Person, String> sortFunction() {
+		return new Function<Person, String>() {
+		  public String apply(Person from) {
+		    return from.getFirstName();
+		  }
+		};
+	}
+	
+	public static void sortingExampleJava8() {
 		List<Person> personnel = createPersonnelList();
 		
 		Comparator<Person> idValueComparator = new Comparator<Person>() {
@@ -60,5 +94,9 @@ public class CollectionFilterExample {
 		List<Person> sortedList = sorted.collect(Collectors.toList());
 		
 		System.out.println(sortedList);
+		// sort by object property via java8
+		personnel.sort(Comparator.comparing(Person::getId));
+		
+		System.out.println(personnel);
 	}
 }
